bcf3b9dd372806e4c44f837c8fefdc9a009b667b
Dimas Irfan <dimasirfan085@gmail.com>
2024-11-18 18:42:18 +0700

    Add chromadb func

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 def load_documents():
     document_loader = PyPDFDirectoryLoader("data")
@@ -27,6 +28,22 @@ def get_embedding_function_aws():
 def get_embedding_function_ollama():
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
+
+def add_to_chroma(chunks: list[Document]):
+    db = Chroma(
+        persist_directory="chroma",
+        embedding_function=get_embedding_function_aws()
+    )
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+    new_chunks = []
+    for chunk in chunks:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+    new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+    db.add_documents(new_chunks, ids=new_chunk_ids)
+    db.persist()
 
 doc = load_documents()
 chunks = split_documents(doc)
