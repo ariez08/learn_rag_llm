543c3a9c4f05cb9c11deb22eb6901819598dc6e2
Dimas Irfan <dimasirfan085@gmail.com>
2024-11-18 18:59:02 +0700

    Adding id to the chunks

--- main.py
+++ main.py
@@ -34,17 +34,46 @@ def add_to_chroma(chunks: list[Document]):
         persist_directory="chroma",
         embedding_function=get_embedding_function_aws()
     )
+
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
     existing_items = db.get(include=[])
     existing_ids = set(existing_items["ids"])
     print(f"Number of existing documents in DB: {len(existing_ids)}")
+    
     new_chunks = []
-    for chunk in chunks:
+    for chunk in chunks_with_ids:
         if chunk.metadata["id"] not in existing_ids:
             new_chunks.append(chunk)
-    new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
-    db.add_documents(new_chunks, ids=new_chunk_ids)
-    db.persist()
 
-doc = load_documents()
-chunks = split_documents(doc)
-print(chunks[0])
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist()
+    else:
+        print("✅ No new documents to add")
+
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # If the page ID is the same as the last one, increment the index.
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        # Calculate the chunk ID.
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # Add it to the page meta-data.
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
