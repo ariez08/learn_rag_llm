f60f28a632172b482a30020e0da4848605188fa4
Dimas Irfan <dimasirfan085@gmail.com>
2024-11-19 14:32:30 +0700

    Deprecation Fix

--- get_embedding.py
+++ get_embedding.py
@@ -1,5 +1,5 @@
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 def get_embedding_function():
     # embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", credentials_profile_name="default", region_name="us-east-1")
